df4b43212c5dcea5d0a6f7e280991754c6363214
Anikate-De <anikate1de@gmail.com>
2022-10-20 06:46:24 +0530

    calendar month ques

---
+++ PROGRAMS/calendar_month.py
@@ -0,0 +1,10 @@
+# Write a Python program to print the calendar of a given month and year.
+
+import calendar
+
+# Get the user input for year and month
+year = int(input('Enter the year - '))
+month = int(input('Enter the two digit number for the month - '))
+   
+# display the calendar
+print(calendar.month(year, month))
