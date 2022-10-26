8c924693035041d89cb8b0a18acf3e2fbb470694
Anikate-De <anikate1de@gmail.com>
2022-10-26 23:28:02 +0530

    celcius to farenheit program

---
+++ PROGRAMS/celcius_farenheit.py
@@ -0,0 +1,6 @@
+# Python Program to Convert Celsius To Fahrenheit
+
+celcius = float(input('Enter the value in Celcius - '))
+
+# We use the formula F = (C * 9/5) + 32
+print(celcius, '°C converts to', (celcius * 9 / 5) + 32,'°F')
