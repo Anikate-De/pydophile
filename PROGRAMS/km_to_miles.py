aa2778b12a4f6b316913be5015d9322da1660b9d
Anikate-De <anikate1de@gmail.com>
2022-10-26 23:24:35 +0530

    kms to miles program

---
+++ PROGRAMS/km_to_miles.py
@@ -0,0 +1,6 @@
+# Python Program to Convert Kilometres to Miles
+
+kms = float(input('Enter the number of kilometres - '))
+
+# We use the formula miles = kms / 1.6
+print(kms, 'Kilometers converts to', kms / 1.6, 'miles')
