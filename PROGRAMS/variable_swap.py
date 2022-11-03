218d9d784700e672eadbecb14e36a042b9fe1d7b
Anikate-De <anikate1de@gmail.com>
2022-11-03 16:23:44 +0530

    updates for week 2

--- PROGRAMS/variable_swap.py
+++ PROGRAMS/variable_swap.py
@@ -8,6 +8,7 @@ print('Value of b before swapping -', b)
 
 # Swapping Logic
 
+print('By using a temporary variable to swap')
 # With temporary variable
 c = a
 a = b
@@ -16,16 +17,18 @@ b = c
 print('Value of a after swapping -', a)
 print('Value of b after swapping -', b)
 
+print('By the method of tuple unpacking')
 # With Tuple unpacking
 a, b = b, a
 
-print('Value of a after swapping -', a)
-print('Value of b after swapping -', b)
+print('Value of a after swapping again -', a)
+print('Value of b after swapping again -', b)
 
+print('By performing mathematical operations')
 # With mathematical operations
 a = b + a
 b = a-b
 a = a-b
 
-print('Value of a after swapping -', a)
-print('Value of b after swapping -', b)
+print('Value of a after swapping again -', a)
+print('Value of b after swapping again -', b)
