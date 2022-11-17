7b70f0ceca06d9d48e75899bce697ad038bdf731
Anikate-De <anikate1de@gmail.com>
2022-11-17 14:45:14 +0530

    update existing programs instead of new ones
    configured the code to separate files.
    
    Co-Authored-By: Bhavyaa <111808599+andbhavyaa@users.noreply.github.com>

--- PROGRAMS/natural_numbers.py
+++ PROGRAMS/natural_numbers.py
@@ -1,8 +1,13 @@
 # Program to print all natural numbers from 1 to n. - using while loop
 
-n = int(input('Enter the value of n - '))
+number = int(input("Please Enter any Number: "))
+i = 1
 
-num = 1
-while num <= n:
-    print(num)
-    num += 1
+print("The List of Natural Numbers from 1 to {0} are".format(number)) 
+
+#Using While Loop we iterate over the range from 1 to n and print the required output.
+
+while ( i <= number):
+    print (i, end = '  ')
+    i = i + 1
+
--- PROGRAMS/odd_sum.py
+++ PROGRAMS/odd_sum.py
@@ -1,13 +1,13 @@
-# Program to find sum of all odd numbers between 1 to n
+# Program to find sum of all odd numbers between 1 to n.
+maximum = int(input(" Please Enter the Maximum Value : "))
+oddTotal = 0
 
-n = int(input('Enter the value of n - '))
+# Using For Loop we give the range between 1 to n
+# to find sum of all odd numbers in that range
 
-# Declare a variable to store the value of the sum
-sum = 0
-num = 2
-while num<n:
-    if not num%2==0:
-        sum+=num
-    num+=1
+for number in range(1, maximum+1):
+    if (number % 2 != 0):
+        print("{0}".format(number))
+        oddTotal = oddTotal + number
 
-print('Sum of all odd numbers BETWEEN 1 AND', n, 'is', sum)
+print("The Sum of Odd Numbers from 1 to {0} = {1}".format(number, oddTotal))
