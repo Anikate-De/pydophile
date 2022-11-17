5bfaf5e3098d70362e23873b3b56966e987884f7
Anikate-De <anikate1de@gmail.com>
2022-11-17 14:34:01 +0530

    update notes in amount program

--- PROGRAMS/notes_in_amount.py
+++ PROGRAMS/notes_in_amount.py
@@ -2,41 +2,52 @@
 
 amt = int(input('Enter the amount - '))
 
-# Create a variable to store the number of notes present in the amount
-notes = 0
+denominations = {
+    2000: 0,
+    500: 0,
+    200: 0,
+    100: 0,
+    50: 0,
+    20: 0,
+    10: 0
+}
 
 # Run a loop till amount is exhausted
-while amt>0:
+while amt > 0:
     # Check if the amount is greater than any of the valid denominations
     # Subtract the denomination and increase notes by 1
-    if amt>=2000:
-        amt-=2000
-        notes+=1
-    elif amt>=1000:
-        amt-=1000
-        notes+=1
-    elif amt>=500:
-        amt-=500
-        notes+=1
-    elif amt>=200:
-        amt-=200
-        notes+=1
-    elif amt>=100:
-        amt-=100
-        notes+=1
-    elif amt>=50:
-        amt-=50
-        notes+=1
-    elif amt>=20:
-        amt-=20
-        notes+=1
-    elif amt>=10:
-        amt-=10
-        notes+=1
+    if amt >= 2000:
+        amt -= 2000
+        denominations[2000] += 1
+    elif amt >= 500:
+        amt -= 500
+        denominations[500] += 1
+    elif amt >= 200:
+        amt -= 200
+        denominations[200] += 1
+    elif amt >= 100:
+        amt -= 100
+        denominations[100] += 1
+    elif amt >= 50:
+        amt -= 50
+        denominations[50] += 1
+    elif amt >= 20:
+        amt -= 20
+        denominations[20] += 1
+    elif amt >= 10:
+        amt -= 10
+        denominations[10] += 1
     else:
         # If the amount is no longer deductible, print the amount
         # As it is present only in coins
         print('Amount present in coins', amt)
         break
 
-print('Number of notes present in the amount is',notes)
+notes = 0
+
+for key, value in denominations.items():
+    if value > 0:
+        print(value, '-', key, 'notes')
+        notes += value
+
+print('Number of notes present in the amount is', notes)
